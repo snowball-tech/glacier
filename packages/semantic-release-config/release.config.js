f8160a8f42d65d54ff0bbf16f38ff459ee2733d2
Clément PRÉVOT <clement@snowball.xyz>
2023-05-16 17:08:14 +0200

    feat: initial commit, release and publication
    This contains ESLint, Prettier, Semantic-Release and Renovate config.
    It also contains an `utils` package to share some utils functions.

---
+++ packages/semantic-release-config/release.config.js
@@ -0,0 +1,53 @@
+// Inspired by https://github.com/oleg-koval/semantic-release-npm-github-publish
+
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        config: '@favoloso/conventional-changelog-emoji',
+        parserOpts: {
+          mergeCorrespondence: ['id', 'source'],
+          mergePattern: /^Merge pull request #(d+) from (.*)$/,
+        },
+        releaseRules: [
+          { release: 'major', type: 'breaking' },
+          { release: 'minor', type: 'feat' },
+          { release: 'patch', type: 'security' },
+          { release: 'patch', type: 'improvement' },
+          { release: 'patch', type: 'perf' },
+          { release: 'patch', type: 'fix' },
+          { release: 'patch', type: 'docs' },
+          { release: 'patch', type: 'doc' },
+          { release: 'patch', type: 'chore' },
+          { release: 'patch', type: 'chores' },
+          { release: 'patch', type: 'refactor' },
+          { release: 'patch', type: 'test' },
+          { release: 'patch', type: 'tests' },
+          { release: 'patch', type: 'style' },
+          { release: 'patch', type: 'code-style' },
+          { release: 'patch', type: 'lint' },
+          { release: 'patch', type: 'build' },
+          { release: 'patch', type: 'deps' },
+          { release: 'patch', type: 'release' },
+          { release: 'patch', type: 'bump' },
+          { release: 'patch', type: 'wip' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'release: new version ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+}
