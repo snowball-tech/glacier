eda7cfc195a5f0bbe2ba700b0b7942447a707e88
Clément PRÉVOT <clement@snowball.xyz>
2023-05-16 17:08:14 +0200

    initial: initialize base packages
    This contains ESLint, Prettier, Semantic-Release and Renovate config.
    It also contains an `utils` package to share some utils functions.

---
+++ packages/eslint-config/configs/testing-library.js
@@ -0,0 +1,27 @@
+const { hasDependency } = require('@snowball-tech/utils')
+
+const hasReact = hasDependency('react')
+
+module.exports = {
+  overrides: [
+    {
+      extends: ['plugin:testing-library/react'],
+
+      // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching testing files!
+      files: ['**/?(*.)+(spec|test)?(s).[jt]s?(x)'],
+
+      rules: {
+        // @testing-library queries throw whenever the element is not found. Because
+        // of that, many tests are written without assertions, but are still safe.
+        'jest/expect-expect': hasReact ? 'off' : 'error',
+
+        'testing-library/no-debugging-utils': 'off',
+        'testing-library/no-node-access': 'off',
+        'testing-library/no-wait-for-side-effects': 'error',
+        'testing-library/prefer-explicit-assert': 'error',
+        'testing-library/prefer-presence-queries': 'error',
+        'testing-library/prefer-query-by-disappearance': 'error',
+      },
+    },
+  ],
+}
---
+++ packages/renovate-config/.lintstagedrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'default.json5': [
+    `yarn renovate-config-validator`,
+    `yarn run -T prettier --ignore-path ../../.prettierignore --write`,
+  ],
+}
