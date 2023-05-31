ab13db5414d3ab378fcb46586c55299f21fc93f1
Clément PRÉVOT <clement@snowball.xyz>
2023-05-31 12:02:09 +0200

    fix(eslint): fix config for NextJS app router with TS

--- packages/eslint-config/configs/next.js
+++ packages/eslint-config/configs/next.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['plugin:@next/next/core-web-vitals'],
+
+  overrides: [
+    {
+      files: ['**/route.ts'],
+      rules: { 'import/prefer-default-export': 'off' },
+    },
+  ],
 }
