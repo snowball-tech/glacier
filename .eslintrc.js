9a0d1a78866d2264c0ee922e2eecca9e60d54c27
Clément PRÉVOT <clement@snowball.xyz>
2023-05-31 14:26:54 +0200

    fix(eslint): fix local eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,15 @@ module.exports = {
   extends: ['@snowball-tech/eslint-config/.eslintrc.js'],
 
   parserOptions: {
+    project: ['./packages/*/tsconfig.json'],
     tsconfigRootDir: __dirname,
+  },
+
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: ['./packages/*/tsconfig.json'],
+      },
+    },
   },
 }
