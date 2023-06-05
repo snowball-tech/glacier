65df3ec2677f95b4c88f462d425768b98d70f8b4
Clément PRÉVOT <clement@snowball.xyz>
2023-06-05 14:30:03 +0200

    feat(prettier): add some useful plugins

--- packages/eslint-config/configs/typescript.js
+++ packages/eslint-config/configs/typescript.js
@@ -10,7 +10,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
 
-      files: ['**/*.ts', '**/*.tsx'],
+      files: ['**/*.ts', '**/*.tsx', '**/*.d.ts'],
 
       overrides: [
         {
