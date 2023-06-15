76c901674cf9a869f58d546f60f1cc12c48ece78
Clément PRÉVOT <clement@snowball.xyz>
2023-06-15 13:51:28 +0200

    fix(eslint): disable some rules in JS fenced code in Markdown

--- packages/eslint-config/markdown.js
+++ packages/eslint-config/markdown.js
@@ -1,5 +1,17 @@
 module.exports = {
   extends: ['plugin:markdown/recommended'],
 
+  overrides: [
+    {
+      files: ['*.md/*.js', '*.mdx/*.js'],
+
+      rules: {
+        'no-undef': 'off',
+        'no-unused-expressions': 'off',
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
+
   plugins: ['markdown'],
 }
