b2e1cd07a2affdea152076b41b4fd484a30be3f6
Clément PRÉVOT <clement@snowball.xyz>
2023-06-15 15:54:06 +0200

    chore: add cache to the format task

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   '!*.{js,cjs,mjs,jsx,ts,tsx,json,json5,jsonc,yml,yaml,md}': [
-    'prettier --ignore-unknown --write',
+    'prettier --ignore-unknown --cache --write',
   ],
   '*.{js,cjs,mjs,jsx,ts,tsx,json,json5,jsonc,yml,yaml,md}': [
     'eslint --cache --fix',
-    'prettier --ignore-unknown --write',
+    'prettier --ignore-unknown --cache --write',
   ],
 }
