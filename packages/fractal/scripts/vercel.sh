02c8a2ce29adf7630736f8a655174eb34084587b
Clément PRÉVOT <clement@snowball.xyz>
2023-06-16 12:21:59 +0200

    build(fractal): try something different to clone freezer

--- packages/fractal/scripts/vercel.sh
+++ packages/fractal/scripts/vercel.sh
@@ -8,9 +8,5 @@
 source "$(dirname "$0")/../../../scripts/colors.sh"
 
 echo -n "$(info "Updating Git config to enforce HTTPS to connected to Freezer... ")"
-git config --global url."https://github.com/snowball-tech/freezer.git".insteadOf "git@github.com:snowball-tech/freezer.git"
-bold_success "DONE"
-
-echo -n "$(info "Updating GH_TOKEN... ")"
-export GH_TOKEN="$SNOWBALL_BOT_TOKEN"
+git config --global url."https://${SNOWBALL_BOT_TOKEN}@github.com/snowball-tech/freezer.git".insteadOf "git@github.com:snowball-tech/freezer.git"
 bold_success "DONE"
