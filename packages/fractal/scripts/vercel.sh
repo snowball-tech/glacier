b2b90ebbc3749bc4f1adc9fc59987668ac950b1c
Clément PRÉVOT <clement@snowball.xyz>
2023-06-16 12:10:13 +0200

    build(fractal): try to use HTTPS + machine token to clone freezer
    Instead of SSH

--- packages/fractal/scripts/vercel.sh
+++ packages/fractal/scripts/vercel.sh
@@ -7,28 +7,10 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")/../../../scripts/colors.sh"
 
-echo -n "$(info "Creating SSH directory... ")"
-mkdir -p "$HOME"/.ssh
+echo -n "$(info "Updating Git config to enforce HTTPS to connected to Freezer... ")"
+git config --global url."https://github.com/snowball-tech/freezer.git".insteadOf "git@github.com:snowball-tech/freezer.git"
 bold_success "DONE"
 
-echo -n "$(info "Creating SSH config file... ")"
-touch "$HOME"/.ssh/config
-chmod 600 "$HOME"/.ssh/config
-bold_success "DONE"
-
-echo "Host github.com
-  Hostname github.com
-  IdentifyFile=$HOME/.ssh/id_freezer" >> "$HOME"/.ssh/config
-
-echo "${REVERSE}"
-cat "$HOME"/.ssh/config
-echo "${NORMAL}"
-
-echo -n "$(info "Adding SSH key... ")"
-echo "$FREEZER_DEPLOY_KEY" | base64 --decode --wrap=0 > "$HOME/.ssh/id_freezer"
-bold_success "DONE"
-
-echo -n "$(info "Cleaning up known hosts... ")"
-ssh-keygen -R "github.com"
-ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
+echo -n "$(info "Updating GH_TOKEN... ")"
+export GH_TOKEN="$SNOWBALL_BOT_TOKEN"
 bold_success "DONE"
