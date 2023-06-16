3704f3f1cbc387121176590eac244c7f30a4b05b
Clément PRÉVOT <clement@snowball.xyz>
2023-06-16 11:03:05 +0200

    build(fractal): add authorization to fetch Freezer in Vercel build

---
+++ packages/fractal/scripts/vercel.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2181
+
+# Setup SSH keys to access GitHub repositories when building from Vercel.
+
+# shellcheck disable=SC1090
+source "$(dirname "$0")/../../../scripts/colors.sh"
+
+mkdir -p "$HOME"/.ssh
+touch "$HOME"/.ssh/config
+chjmod 600 "$HOME"/.ssh/config
+
+echo """
+Host github.com
+  Hostname github.com
+  IdentifyFile=$HOME/.ssh/id_freezer
+""" >> ~/.ssh/config
+
+echo "$FREEZER_DEPLOY_KEY" | base64 --decode --wrap=0 > "$HOME/.ssh/id_freezer"
+
+ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
