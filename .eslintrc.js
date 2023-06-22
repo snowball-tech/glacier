56c1719b5bd5b4789bcbe3a3b06abd0504c89a85
Clément PRÉVOT <clement@snowball.xyz>
2023-06-22 18:42:48 +0200

    chore: update docs and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,33 @@ require('@snowball-tech/eslint-config/patch')
 module.exports = {
   extends: [
     '@snowball-tech/eslint-config/base',
+    '@snowball-tech/eslint-config/html',
     '@snowball-tech/eslint-config/json',
     '@snowball-tech/eslint-config/markdown',
     '@snowball-tech/eslint-config/yml',
     '@snowball-tech/eslint-config/secrets',
     '@snowball-tech/eslint-config/import',
+    '@snowball-tech/eslint-config/typescript',
     '@snowball-tech/eslint-config/lodash',
     '@snowball-tech/eslint-config/perfectionist',
     '@snowball-tech/eslint-config/prettier',
   ],
 
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+
   root: true,
+
+  settings: {
+    'import/resolver': {
+      [require.resolve('eslint-import-resolver-typescript')]: {
+        alwaysTryTypes: true,
+        typescript: {
+          project: ['./tsconfig.json'],
+        },
+      },
+    },
+  },
 }
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  '!*.{js,cjs,mjs,jsx,ts,tsx,json,json5,jsonc,yml,yaml,md}': [
+  '!*.{js,cjs,mjs,jsx,ts,tsx,json,json5,jsonc,yml,yaml,md,mdx,css,html}': [
     'prettier --ignore-unknown --cache --write',
   ],
-  '*.{js,cjs,mjs,jsx,ts,tsx,json,json5,jsonc,yml,yaml,md}': [
+  '*.{js,cjs,mjs,ts,json,json5,jsonc,yml,yaml,md}': [
+    'eslint --cache --fix',
+    'prettier --ignore-unknown --cache --write',
+  ],
+  '*.{jsx,tsx,mdx,css,html}': [
+    'stylelint --cache --fix',
     'eslint --cache --fix',
     'prettier --ignore-unknown --cache --write',
   ],
