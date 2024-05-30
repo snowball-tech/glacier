5a1cb4a452e53fb145143a8db10282fa55d2e3ef
Clément PRÉVOT <clement@snowball.xyz>
2024-05-30 08:03:24 +0000

    fix(eslint-config): allow nested ternary in react applications

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -15,7 +15,12 @@ module.exports = {
 
   rules: {
     'class-methods-use-this': 'off',
+    // It can be useful to nest ternary in React component render.
+    'no-nested-ternary': 'off',
 
+    // Allow autofocus even if it's not perfectly A11Y, it's so useful for
+    // users.
+    // eslint-disable-next-line sort-keys/sort-keys-fix
     'jsx-a11y/no-autofocus': 'off',
 
     'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
