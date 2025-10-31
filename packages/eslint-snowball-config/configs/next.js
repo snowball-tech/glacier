7d1330d4c59d36827f7bcd4ee62887899d749ec6
Clément PRÉVOT <clement@snowball.xyz>
2025-10-31 15:36:45 +0100

    fix(eslint): update next config

--- packages/eslint-snowball-config/configs/next.js
+++ packages/eslint-snowball-config/configs/next.js
@@ -1,37 +1,37 @@
-import getCompatConfig from '../compat.js'
+import eslintConfigNext from 'eslint-config-next/dist'
+import nextVitals from 'eslint-config-next/dist/core-web-vitals'
 
-const compatConfig = getCompatConfig(
-  ['plugin:@next/next/core-web-vitals'],
-  ['@next/next'],
-  [
-    {
-      name: 'next-custom',
+export default [
+  {
+    name: 'next-custom',
 
-      ignores: ['**/.next/*', '**/.well-known'],
+    files: ['**/*.{js,mjs,cjs,jsx,ts,mts,tsx}'],
 
-      rules: {
-        '@next/next/no-html-link-for-pages': 'off',
+    ignores: ['**/.next/*', '**/.well-known'],
 
-        'jsx-a11y/alt-text': [
-          'warn',
-          {
-            elements: ['img'],
-            img: ['Image'],
-          },
-        ],
-        'jsx-a11y/aria-props': 'warn',
-        'jsx-a11y/aria-proptypes': 'warn',
-        'jsx-a11y/aria-unsupported-elements': 'warn',
-        'jsx-a11y/role-has-required-aria-props': 'warn',
-        'jsx-a11y/role-supports-aria-props': 'warn',
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
 
-        'react/jsx-no-target-blank': 'off',
-        'react/no-unknown-property': 'off',
-        'react/prop-types': 'off',
-      },
+      'jsx-a11y/alt-text': [
+        'warn',
+        {
+          elements: ['img'],
+          img: ['Image'],
+        },
+      ],
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+
+      'react/jsx-no-target-blank': 'off',
+      'react/no-unknown-property': 'off',
+      'react/prop-types': 'off',
     },
-  ],
-  ['**/*.{js,mjs,cjs,jsx,ts,mts,tsx}'],
-)
+  },
 
-export default compatConfig
+  ...eslintConfigNext,
+
+  ...nextVitals,
+]
